9d625df68384152fd940edf22a1bb7d52c521fd3
iliya <ipvolkov.fitm22@kubg.edu.ua>
2026-03-05 00:14:32 +0200

    fix: make .env path absolute to resolve IDE sources root issues

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,9 @@
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
 
 class TelegramSettings(BaseModel):
     bot_token: str
@@ -12,14 +15,13 @@ class KafkaSettings(BaseModel):
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=".env",
+        env_file=BASE_DIR / ".env",
         case_sensitive=False,
-        env_file_encoding="utf-8"
-        # env_nested_delimiter="__",
-        # env_prefix="APP_CONFIG__"
+        env_file_encoding="utf-8",
+        env_nested_delimiter="__",
     )
-    tg: TelegramSettings = TelegramSettings()
-    kafka: KafkaSettings = KafkaSettings()
+    tg: TelegramSettings
+    kafka: KafkaSettings
 
 
 settings = Settings()
--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ from core.config import settings
 async def consume_kafka():
     consumer = AIOKafkaConsumer(
         'my_topic', 'my_other_topic',
-        bootstrap_servers='localhost:9092',
+        bootstrap_servers=settings.kafka.kafka_bootstrap_servers,
         group_id="my-group")
 
     await consumer.start()
@@ -21,17 +21,18 @@ async def consume_kafka():
 
 
 async def main():
-    bot = Bot(token=settings.bot_token)
+    bot = Bot(token=settings.tg.bot_token)
     disp = Dispatcher()
-
+    # disp.include_router(router)
     kafka_task = asyncio.create_task(consume_kafka())
 
     print("Bot is starting...")
 
-    await disp.start_polling(bot)
-
-
-asyncio.run(consume_kafka())
+    try:
+        await disp.start_polling(bot)
+    finally:
+        await disp.stop_polling()
+        kafka_task.cancel()
 
 
 if __name__ == "__main__":
